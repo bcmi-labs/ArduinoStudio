714df719ab51e9a5c995b14f3e714e08cd2e63c9
quasto <sergio@arduino.org>
2015-06-09 17:57:11 +0200

    moved healthdata extesnions

---
+++ src/extensions/default/test/main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by sergio on 05/06/15.
+ */
+
+define(function (require, exports, module) {
+    "use strict";
+
+    // Brackets modules
+    var arduinoSergio         = brackets.getModule("arduino/Sergio");
+    var arduinoCMD            = brackets.getModule("arduino/Commands");
+
+    arduinoSergio.test();
+
+
+});
