becf3aef3927c839662c4b01f623df31367a3f56
quasto <sergio@arduino.org>
2015-09-04 11:32:11 +0200

    first integration step of arduino console into the studio core

---
+++ src/arduino/Console.js
@@ -0,0 +1,121 @@
+/*
+ * This file is part of Arduino Studio
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Copyright 2015 Arduino Srl (http://www.arduino.org/) support@arduino.org
+ *
+ * authors:     sergio@arduino.org
+ * date:        04 Sep 2015
+ */
+
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+
+define(function (require, exports, module) {
+    "use strict";
+
+    // Load dependent modules
+    var AppInit           = require("utils/AppInit"),
+        ConsoleView       = require("arduino/ConsoleView").ConsoleView;
+
+    // make sure the global brackets variable is loaded
+    require("utils/Global");
+
+
+    /** @type {ConsoleView} The console view. Initialized in htmlReady() */
+    var _consoleView = null;
+
+    /**
+     * Hides the Console Panel
+     */
+    function hide() {
+        _consoleView.hide();
+    }
+
+    /**
+     * Shows the Console Panel
+     */
+    function show() {
+        _consoleView.show();
+    }
+
+    /**
+     * Clears the Console Panel
+     */
+    function clear() {
+        _consoleView.clear();
+    }
+
+    /**
+     * Log an Info message on the Console Panel
+     *
+     * @param {Object} data The message to log on the Console.
+     */
+    function logInfo(data) {
+        if(data) {
+            _consoleView.logInfo(JSON.stringify(data));
+        }
+    }
+
+    /**
+     * Log an Error message on the Console Panel
+     *
+     * @param {Object} data The message to log on the Console.
+     */
+    function logError(data) {
+        if(data) {
+            _consoleView.logError(JSON.stringify(data));
+        }
+    }
+
+    /**
+     * Log a Success/OK message to the Console Panel
+     *
+     * @param {Object} data The message to log on the Console.
+     */
+    function logSuccess(data) {
+        if(data) {
+            _consoleView.logSuccess(JSON.stringify(data));
+        }
+    }
+
+    AppInit.htmlReady(function () {
+        //TODO set names
+        _consoleView = new ConsoleView("arduino-console");
+        /*        var model = FindInFiles.searchModel;
+                _resultsView = new SearchResultsView(model, "find-in-files-results", "find-in-files.results");
+                _resultsView
+                    .on("replaceAll", function () {
+                        _finishReplaceAll(model);
+                    })
+                    .on("close", function () {
+                        FindInFiles.clearSearch();
+                    });*/
+    });
+
+
+    // Define public API
+    exports.show = show;
+    exports.hide = hide;
+    exports.clear = clear;
+    exports.logInfo = logInfo;
+    exports.logError = logError;
+    exports.logSuccess = logSuccess;
+});
---
+++ src/arduino/ConsoleView.js
@@ -0,0 +1,135 @@
+/*
+ * This file is part of Arduino Studio
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Copyright 2015 Arduino Srl (http://www.arduino.org/) support@arduino.org
+ *
+ * authors:     sergio@arduino.org
+ * date:        04 Sep 2015
+ *
+ */
+
+
+/*global define, $, Mustache */
+
+/*
+ * Panel showing the log Console for Compile/Upload operations. more in general for log messages.
+ */
+define(function (require, exports, module) {
+    "use strict";
+
+    var
+        EventDispatcher       = require("utils/EventDispatcher"),
+        WorkspaceManager      = require("view/WorkspaceManager"),
+        consolePanelTemplate  = require("text!htmlContent/console-panel.html");
+
+    /**
+     * @constructor
+     * Handles the Console panel.
+     *
+     * @param {string} panelName The name to use for the panel, as passed to PanelManager.createBottomPanel().
+     */
+    function ConsoleView(panelName) {
+        var panelHtml  = Mustache.render(consolePanelTemplate, {});//TODO check if need to pass some parameters in mustache.render
+
+        this._panel    = WorkspaceManager.createBottomPanel(panelName, $(panelHtml));
+        this._$logger  = this._panel.$panel.find("#logger");
+        //this._model    = model;
+
+        //TODO check in preferences if console panel should be showed
+        //this._panel.show();
+    }
+    EventDispatcher.makeEventDispatcher(ConsoleView.prototype);
+
+    /** @type {ConsoleModel} The Console model we're viewing. */
+    //ConsoleView.prototype._model = null;
+
+    /** @type {Panel} Bottom panel holding the Console */
+    ConsoleView.prototype._panel = null;
+
+    /** @type {$.Element} The element where the console log is placed */
+    ConsoleView.prototype._$logger = null;
+
+    /**
+     * Shows the Console panel.
+     */
+    ConsoleView.prototype.show = function () {
+        if(this._panel && !this._panel.isVisible()){
+            this._panel.show();
+            this.trigger("show");
+        }
+    };
+
+    /**
+     * Hides the Console Panel.
+     */
+    ConsoleView.prototype.hide = function () {
+        if (this._panel && this._panel.isVisible()) {
+            this._panel.hide();
+            this.trigger("hide");
+        }
+    };
+
+    /**
+     * Clears the Console Log panel.
+     */
+    ConsoleView.prototype.clear = function () {
+        if (this._panel && this._$logger) {
+            this._$logger.empty();
+            this.trigger("clear");
+        }
+    };
+
+    /**
+     * Appends Info log messages into the Console.
+     *
+     * @param {string} message The message to log on Console.
+     */
+    ConsoleView.prototype.logInfo = function (message) {
+        if (this._panel && this._$logger) {
+            this._$logger.append("[" + new Date().toLocaleString() + "] - <span style='color: black;'>" + message + "</span><br />");
+        }
+    };
+
+    /**
+     * Appends Error log messages into the Console.
+     *
+     * @param {string} message The message to log on Console.
+     */
+    ConsoleView.prototype.logError = function (message) {
+        if (this._panel && this._$logger) {
+            this._$logger.append("[" + new Date().toLocaleString() + "] - <span style='color: darkred;'>" + message + "</span><br />");
+        }
+    };
+
+    /**
+     * Appends Success/OK log messages into the Console.
+     *
+     * @param {string} message The message to log on Console.
+     */
+    ConsoleView.prototype.logSuccess = function (message) {
+        if (this._panel && this._$logger) {
+            this._$logger.append("[" + new Date().toLocaleString() + "] - <span style='color: darkgreen;'>" + message + "</span><br />");
+        }
+    };
+
+    // Public API
+    exports.ConsoleView = ConsoleView;
+});
