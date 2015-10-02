4637136c9ae36a30678dd2a742417d2cc6820a21
quasto <sergio@arduino.org>
2015-10-02 18:27:19 +0200

    serial port wrapper

--- src/arduino/SerialPort.js
+++ src/arduino/SerialPort.js
@@ -31,62 +31,142 @@ define(function (require, exports, module) {
     "use strict";
 
     // Load dependent modules
-    var //AppInit             = require("utils/AppInit"),
-        NodeDomain          = require("utils/NodeDomain"),
-        FileUtils           = require("file/FileUtils,");
+    var NodeDomain          = require("utils/NodeDomain"),
+        FileUtils           = require("file/FileUtils"),
+        EventDispatcher     = require("utils/EventDispatcher");
 
-    var domainPath = FileUtils.getNativeBracketsDirectoryPath() + "/" + FileUtils.getNativeModuleDirectoryPath(module) + "/node/serialport";
-    //var NodeSerialPort      = new NodeDomain("nodeSerialPort", domainPath);
 
-    //PreferencesManager.definePreference("arduino.serialMonitorAutoScroll", "boolean", true);
-    //PreferencesManager.definePreference("arduino.serialM", "boolean", true);
+    var domainPath = FileUtils.getNativeBracketsDirectoryPath() + "/" + FileUtils.getNativeModuleDirectoryPath(module) + "/node/SerialDomain";
+    var serialDomain      = new NodeDomain("arduino-serial-domain", domainPath);
+
+    //var EOL = {
+    //    NL:     "\n",
+    //    CR:     "\r",
+    //    BOTH:   "\r\n"
+    //};
 
     /**
      * @constructor
      * Create a SerialPort object.
      *
-     * @param {String} address Identification of serial port (e.g. COM1, tty0)
+     * @param {String} address Identification of serial port (e.g. COM1, /dev/tty0)
      * @param {Number} Baud Rate Baud rate for serial connection
-     * @param {String} eol End Of Line char
      */
-    function SerialPort(address, baudRate, eol) {
-
+    function SerialPort(address, baudRate) {
         this.address = address;
         this.baudRate = baudRate || 9600;
-        this.eol = eol || "NL";
+
+        var that = this;
+        //TODO manage the promise callback - done and fail
+        serialDomain.exec("create", this.address, parseInt(this.baudRate, 10));
+
+        this._handleSerialData = function($evt, port, data){
+            if(that.address.localeCompare(port) === 0 ) {
+                that.trigger("data", data);
+            }
+        };
+
+        serialDomain.on('data', this._handleSerialData);
+
+        //function _handleSerialData($evt, port, data){
+        //    if(that.address.localeCompare(port) == 0 ) {
+        //        that.trigger("data", data);
+        //    }
+        //}
     }
+
+    EventDispatcher.makeEventDispatcher(SerialPort.prototype);
+
+    /**
+     * Opens the connection to serial port, only if connection is not already opened
+     *
+     * @return {jQuery.Promise} this promise resolve true when the serial port is opened or reject an error.
+     */
+    SerialPort.prototype.open = function(){
+        var $deferred = $.Deferred(),
+            that = this;
+
+        serialDomain.exec("isOpen", that.address)
+            .done( function(status){
+                if(status){
+                    $deferred.resolve(status);
+                }
+                else{
+                    serialDomain.exec("open", that.address)
+                        .done( function(result){
+                            $deferred.resolve(true);
+                        })
+                        .fail(function(error) {
+                            $deferred.reject(error);
+                        });
+                }
+            })
+            .fail(function(error) {
+                $deferred.reject(error);
+            });
+
+        return $deferred.promise();
+    };
 
     /**
      * Closes the connection to the serial port
+     *
+     * @return {promise}
      */
     SerialPort.prototype.close = function(){
-        //TODO
-    };
+        var $deferred = $.Deferred();
+        serialDomain.exec("close", this.address)
+            .done( function(result){
+                $deferred.resolve(result);
+            })
+            .fail(function(error) {
+                $deferred.reject(error);
+            });
 
-    /**
-     * Opens the connection to serial port
-     */
-    SerialPort.prototype.open = function(){
-        //TODO
-    };
-
-    /**
-     * Says if the connection is open
-     */
-    SerialPort.prototype.isOpen = function(){
-        //TODO
+        serialDomain.off("data");       //remove listener between this and node domain
+        this.off("data");               //remove listeners between this and user space
+        return $deferred.promise();
     };
 
     /**
      * Sends message trough the serial connection
+     *
+     * @param {String} message The message to transmit via serial connection
      */
-    SerialPort.prototype.send = function(){
-        //TODO
+    SerialPort.prototype.write = function(message){
+        var $deferred = $.Deferred();
+        if(message) {
+            serialDomain.exec("write", this.address, message)
+                .done( function(result){
+                    $deferred.resolve(result);
+                })
+                .fail(function(error) {
+                    $deferred.reject(error);
+                });
+        }
+        else{
+            //TODO locate this message
+            $deferred.reject("Empty message");
+        }
+        return $deferred.promise();
     };
 
-    //AppInit.htmlReady(function () {
-    //
-    //});
+    /**
+     * Says if the connection is open
+     *
+     * @return {promise}
+     */
+    SerialPort.prototype.isOpen = function(){
+        var $deferred = $.Deferred();
+        serialDomain.exec("isOpen", this.address)
+            .done( function(status){
+                $deferred.resolve(status);
+            })
+            .fail(function(err) {
+                $deferred.reject(err);
+            });
+        return $deferred.promise();
+    };
 
     // Public API
     exports.SerialPort = SerialPort;
